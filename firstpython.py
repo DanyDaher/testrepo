9975721f58304310b487b91a52cd71372cfd1a53
DanyDaher <46608246+DanyDaher@users.noreply.github.com>
2023-12-20 09:43:36 -0500

    Update firstpython.py

--- firstpython.py
+++ firstpython.py
@@ -1,3 +1,14 @@
 # Display the output
 
 print('hello world')
+
+import requests
+import pandas as pd
+import numpy as np
+import datetime
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_colwidth', None)
+
+
+
